7a06955a80cc5df5f8a3d563e70847de7c2aa30a
HmsGoBrr <metalcorehms@gmail.com>
2021-11-29 06:26:22 +0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { Router, Route } from "svelte-navigator";
+    import Home from "./views/Home.svelte";
+</script>
+
+<Router>
+    <Route path="/">
+        <Home />
+    </Route>
+    <Route path="/play">
+        <h1>ae.</h1>
+    </Route>
+</Router>
+
+<style>
+</style>
