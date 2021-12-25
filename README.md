10a684ce743b75394aa75a065fe715fcd70dd7f5
HmsGoBrr <metalcorehms@gmail.com>
2021-12-25 20:00:51 +0700

    clear master branch

--- README.md
+++
@@ -1,5 +0,0 @@
-# Country Quiz
-
-**This project is in W.I.P**\
-\
-Quiz with [Country API](https://restcountries.com/)
--- src/views/Home.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-</script>
-
-<h1>Country Quiz</h1>
-<p>Quiz about countries with <a href="http://restcountries.com">Countries API</a></p>
-
-<style>
-</style>
